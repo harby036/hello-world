f5bb242ca7b634218ba05ff4ecceb90c89e35a22
harby036 <40007686+harby036@users.noreply.github.com>
2018-06-07 07:56:54 +1000

    Update and rename README.md to README.py

---
+++ README.py
@@ -0,0 +1,12 @@
+def calc():
+    var1 = int(input('this is a test code to see if it works'))
+    var2 = int(input('place the second option'))
+    ans = var1 * var2
+    return ans
+run = 'y'
+while run == 'y'
+    run = input('would you like to use the test method')
+    if run not in 'y' or 'n':
+            run = input('please try again either [y|n]')
+    calc()
+print('bye')
